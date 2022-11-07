d20c780451ddb646bcbc783d9f51cbec67c0c87d
Andrew McKean <a.mckean90@outlook.com>
2022-11-07 18:01:33 +0000

    Added weather icons, updated contentboxes updated dashboard presentation

---
+++ src/components/DashGreeting.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import './index.css';
+import axios from "axios";
+import jwt from 'jwt-decode';
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+const token = cookies.get("TOKEN");
+
+export default function Greeting() {
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    // set configurations for the API call here
+    const configuration = {
+      method: "get",
+      url: "https://challenge-auth-app.herokuapp.com/dashboard",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    axios(configuration)
+      .then((result) => {
+        const user = jwt(token);
+        setUsername(user.userName);
+      })
+      .catch((error) => {
+        error = new Error();
+      })
+  }, [])
+
+  return (
+      <h1 className="dashGreeting text-center">Good day {username}</h1>
+  );
+}
