4b83de2274f9c32f56027f260b358ea7347ec711
Andrew McKean <a.mckean90@outlook.com>
2022-11-09 14:09:56 +0000

    Added sports.csv for sports module Corrected DOM errors

--- src/components/SportBox.js
+++ src/components/SportBox.js
@@ -5,28 +5,34 @@ import './index.css';
 
 export default function PieBox(props) {
   return (
+    <Container>
     <Card className="contentBox" style={{width: '20rem', height: '15rem', background: 'rgba(218, 223, 225, 0.5)'}}>
       <Card.Header style={{background: '#fdfd96', textAlign: 'center', fontSize: '120%', fontWeight: 'bold'}}>
         {props.cardTitle}
       </Card.Header>
-      <Row className="d-flex align-items-center justify-content-center">
-        Colour: Garment: Days worn
-      </Row>
-      <Row className="d-flex align-items-center justify-content-center">
+      <Card.Text className="text-center">
+        <Row>
+          <Card.Text>Colour: Garment: Days worn<br /></Card.Text>
+        </Row>
+        <Row>
         <Col style={{width: '50%'}}>
           {props.pie}
         </Col>
         <Col style={{fontSize: '90%', justifyContent: 'flex-start'}}>
-
-            &nbsp;Teal: Blazer: 146<br />
-            &nbsp;Green: Raincoat: 171 <br />
-            &nbsp;Gray: Jumper: 189 <br />
-            &nbsp;Purple: Hoodie: 153 <br />
-            &nbsp;Yellow: Jacket: 171 <br />
-            &nbsp;Orange: Sweater: 170 <br />
-
+          <Row>
+            <Card.Text>
+              &nbsp;Teal: Blazer: 146<br />
+              &nbsp;Green: Raincoat: 171 <br />
+              &nbsp;Gray: Jumper: 189 <br />
+              &nbsp;Purple: Hoodie: 153 <br />
+              &nbsp;Yellow: Jacket: 171 <br />
+              &nbsp;Orange: Sweater: 170 <br />
+            </Card.Text>
+          </Row>
         </Col>
-      </Row>
+        </Row>
+      </Card.Text>
     </Card>
+    </Container>
   )
 }
