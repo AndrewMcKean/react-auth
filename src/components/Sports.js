faf2bd280843c122b21a947672f6e54772d65b9b
Andrew McKean <a.mckean90@outlook.com>
2022-11-09 17:14:21 +0000

    Added style to sports page

--- src/components/Sports.js
+++ src/components/Sports.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, Form, ListGroup } from 'react-bootstrap';
 import Logout from './Logout';
 
@@ -6,7 +6,6 @@ import Logout from './Logout';
 
 export default function Sports(props) {
   const [team, setTeam] = useState("");
-  const [message, setMessage] = useState("");
   const [validTeam, setValidTeam] = useState(false);
   const [loserTeams, setLoserTeams] = useState([]);
 
@@ -21,6 +20,7 @@ export default function Sports(props) {
       setLoserTeams(sportsTeamsJson[team]);
     } else {
       setValidTeam(false);
+      setLoserTeams("");
     }
 
 
@@ -33,47 +33,39 @@ export default function Sports(props) {
         <Col className="d-flex align-items-center justify-content-start" sm={8}>
           <h1 style={{color: 'white', fontSize: '5em'}}>Sports</h1>
         </Col>
-        <Col className="d-flex align-items-center justify-content-end logoutContainer" sm={2}>
+        <Col className="d-flex align-items-center justify-content-end logoutContainer" sm={4}>
           <Logout />
         </Col>
       </Row>
-      {/*News Image - Unable to source story image from rss*/}
-      <Row className="align-items-center"> 
-        <h2 className="text-center">Enter an Italia Ligue 1 sports team to find out who they beat!</h2>
-      </Row>  
-      <Row className="contentRow">
-        <Col className="d-flex align-items-top justify-content-center newsExtendedImg">
-          <Form>
-            <Form.Control 
-              className="bg-transparent"
-              type="email"
-              name="email"
-              size="lg"
-              value={team}
-              onChange={(e) => setTeam(e.target.value)}
-              onBlur={(e) => findTeams(e)}
-              placeholder="Enter team..." 
-            />
-            {message}
-          </Form>
-        </Col>
-        <div className="imageList" style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
-        {validTeam ? <p>These are the teams that {team} beat.</p> : 
-          <p>{team} is not a valid team, try again.</p>}
+      {/*News Image - Unable to source story image from rss*/} 
+      <Col className="d-block" stlye={{}}>
+        <Row>
+          <Form.Control 
+            className="bg-transparent"
+            style={{maxWidth: '60%', marginLeft: 'auto', marginRight: 'auto'}}
+            type="text"
+            name="text"
+            size="lg"
+            value={team}
+            onChange={(e) => setTeam(e.target.value)}
+            onBlur={(e) => findTeams(e)}
+            placeholder="Enter winning Serie A team..." 
+          />
+          {validTeam ? <p className="text-center">These are the teams that {team} beat.</p> : 
+          team ? <p>{team} is not a valid team, try again.</p> : null}
+          {/*If state contains loserTeams, display them*/}
+        </Row>
+      </Col>
+      <div className="imageList" style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
         {/*If state contains loserTeams, display them*/}
         {loserTeams ? Object.entries(loserTeams).map((entry) => {
-          return (
-            <ListGroup>
-              <ListGroup.Item>{entry[1]}</ListGroup.Item>
-            </ListGroup>
-          ) 
-        }) : null}
+            return (
+              <ListGroup horizontal variant="">
+                <ListGroup.Item style={{margin: '0.5em'}}>{entry[1]}</ListGroup.Item>
+              </ListGroup>
+            ) 
+              }) : null}
       </div>
-      </Row>
-      {/*News description*/}
-      <Row className="align-items-center">
-
-      </Row>
     </Container>
   )
 
